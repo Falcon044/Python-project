f9ec2454da9ece68501edb99cc53e49c1e83260f
dedokTV <greatfalcon044@gmail.com>
2023-02-28 14:47:55 +0200

    Created initial version of calculator.py
    - Added 4 simple actions +, -, *, /

---
+++ calculator.py
@@ -0,0 +1,37 @@
+print("Calculator")
+print("Available options: +, -, *, /")
+
+first_num = int(input("Enter first number: "))
+action = input("Choose action for calculation: ")
+second_num = int(input("Enter second number: "))
+
+
+def sum(first_num, second_num):
+    return first_num + second_num
+
+
+def subt(first_num, second_num):
+    return first_num - second_num
+
+
+def mult(first_num, second_num):
+    return first_num * second_num
+
+
+def div(first_num, second_num):
+    return first_num / second_num
+
+
+def execution(action):
+    if action == "+":
+        print(str(sum(first_num, second_num)))
+    elif action == "-":
+        print(str(subt(first_num, second_num)))
+    elif action == "*":
+        print(str(mult(first_num, second_num)))
+    elif action == "/":
+        print(str(div(first_num, second_num)))
+
+
+if __name__ == "__main__":
+    execution(action)
